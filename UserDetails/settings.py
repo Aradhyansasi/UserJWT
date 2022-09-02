0d9161b8830c18008433c36f0c6ebf5d9cbbde31
Aradhya N Sasi <aradhya.n.sasi101@gmail.com>
2022-09-02 13:28:14 +0530

    database changed to postgreSQL

--- UserDetails/settings.py
+++ UserDetails/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'UserDetails.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'usersAuth',
+        'USER': 'postgres',
+        'PASSWORD': 'dragon',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
